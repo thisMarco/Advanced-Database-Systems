98485c38252d12a984ac108c12f59cd4b76973e8
Marco <55879616+thisMarco@users.noreply.github.com>
2020-03-29 16:34:12 +0100

    Completed Scripts

---
+++ DROPS.sql
@@ -0,0 +1,34 @@
+/*  TABLES AND TYPES DROPS  */
+
+/*  TABLES  */
+DROP TABLE tabCustomersAccounts;
+/
+DROP TABLE tabAccounts;
+/
+DROP TABLE tabBranches;
+/
+DROP TABLE tabEmployees;
+/
+DROP TABLE tabCustomers;
+/
+
+/*  TYPES   */
+DROP TYPE typAccount;
+/
+DROP TYPE typEmployee;
+/
+DROP TYPE typCustomer;
+/
+DROP TYPE typPerson;
+/
+DROP TYPE typBranch;
+/
+DROP TYPE typPhonesTable;
+/
+DROP TYPE typPhone;
+/
+DROP TYPE typAddress;
+/
+DROP TYPE typName;
+
+--purge dba_recyclebin;
